9bf21b2fe6ad2c78e30eb028a316aa24f3eea40c
pengoox <amaanabbasi99@gmail.com>
2018-07-27 18:43:24 +0530

    admin-panel-added

--- src/community_site/community_site/urls.py
+++ src/community_site/community_site/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "NEIL & AMAAN"
+admin.site.site_title = "NEIL & AMAAN Admin Portal"
+admin.site.index_title = "Welcome to NEIL & AMAAN Researcher Portal"
